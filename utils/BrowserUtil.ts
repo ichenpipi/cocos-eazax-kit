486f1556737253c11c9d911be9d3084ab5de13e9
陈皮皮 <952157129@qq.com>
2020-08-03 18:20:54 +0800

    update utils/BrowserUtil.ts.

--- utils/BrowserUtil.ts
+++ utils/BrowserUtil.ts
@@ -1,45 +1,44 @@
 export default class BrowserUtil {
 
     /**
-     * 复制数据至设备剪贴板
-     * @param value 内容
+     * 复制文本至设备剪贴板
+     * @param value 文本内容
      */
     public static copy(value: string) {
-        return new Promise<boolean>(res => {
-            if (document.location.protocol === 'https:' || document.location.hostname === 'localhost' || document.location.hostname === '127.0.0.1') {
-                // navigator.clipboard API 只适用于安全连接
-                console.log('Copy via navigator.clipboard');
-                navigator.clipboard.writeText(value)
-                    .then(() => {
-                        console.log('Clipboard write success!');
-                        res(true);
-                    })
-                    .catch(err => {
-                        console.error('Clipboard write failed:', err);
-                        res(false);
-                    });
-            } else {
-                console.log('Copy via document.execCommand');
-                // 创建输入元素
-                let element = document.createElement('input');
-                element.readOnly = true;
-                element.style.opacity = '0';
-                element.value = value;
-                document.body.appendChild(element);
-                // 选择元素
-                element.select();
-                // 低版本 iOS 特殊处理
-                let range = document.createRange();
-                range.selectNodeContents(element);
-                let selection = getSelection();
-                selection.removeAllRanges();
-                selection.addRange(range);
-                // 复制
-                let result = document.execCommand('copy');
-                element.remove();
-                res(result);
-            }
-        });
+        // 创建输入元素
+        let element = document.createElement('textarea');
+        element.readOnly = true;
+        element.style.opacity = '0';
+        element.value = value;
+        document.body.appendChild(element);
+        // 选择元素
+        element.select();
+        // 兼容低版本 iOS 的特殊处理
+        let range = document.createRange();
+        range.selectNodeContents(element);
+        let selection = getSelection();
+        selection.removeAllRanges();
+        selection.addRange(range);
+        // 复制
+        let result = document.execCommand('copy');
+        element.remove();
+        return result;
+        // return new Promise<boolean>(res => {
+        //     if (document.location.protocol === 'https:' || document.location.hostname === 'localhost' || document.location.hostname === '127.0.0.1') {
+        //         // navigator.clipboard API 只适用于安全连接
+        //         console.log('Copy via navigator.clipboard');
+        //         navigator.clipboard.writeText(value)
+        //             .then(() => {
+        //                 console.log('Clipboard write success!');
+        //                 res(true);
+        //             })
+        //             .catch(err => {
+        //                 console.error('Clipboard write failed:', err);
+        //                 res(false);
+        //             });
+        //     } else {
+        //         console.log('Copy via document.execCommand');
+        //     }
+        // });
     }
-
 }

5b5f1feec42844ef6b3172a92bbcd9e3ec872195
陈皮皮 <952157129@qq.com>
2020-09-06 23:13:53 +0800

    update misc/EditorAsset.ts.

--- misc/EditorAsset.ts
+++ misc/EditorAsset.ts
@@ -1,21 +1,24 @@
+/**
+ * 编辑器内资源加载类
+ * @see EditorAsset.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/misc/EditorAsset.ts
+ */
 export default class EditorAsset {
 
     /**
      * 在编辑器环境中加载 assets 目录下的资源
-     * @param url 相对路径，根目录为 ’asssts/‘，注意带上文件后缀
-     * @param type 资源类型
-     * @param callback 完成回调
+     * @param url 相对路径，根目录为 assets/，注意带上文件后缀，如：eazax-sine-wave.effect
+     * @param type 资源类型，如：effect
+     * @param callback 加载完成回调
      * @example 
-     *  EditorAsset.load('eazax-ccc/resources/effects/eazax-hollowout.effect', 'effect', (err: any, result: cc.EffectAsset) => {
+     *  EditorAsset.load('eazax-ccc/resources/effects/eazax-sine-wave.effect', 'effect', (err: Error, result: cc.EffectAsset) => {
      *      this.effect = result;
      *  });
      */
-    static load<T>(url: string, type: string, callback: (err: Error, result: T) => void): void {
-        if (!CC_EDITOR) return;
+    public static load<T>(url: string, type: string, callback: (err: Error, result: T) => void): void {
+        if (!CC_EDITOR) return cc.warn('[EditorAsset]', '该函数只在编辑器环境内有效！');
         Editor.assetdb.queryAssets(`db://assets/${url}`, type, (err: Error, results: AssetInfo[]) => {
-            if (err) return callback(new Error('[EditorAsset] Unknow error!'), null);
-            if (results.length === 0) return callback(new Error('[EditorAsset] No asset found!'), null);
-
+            if (err) return callback(new Error('[EditorAsset] 未知错误!'), null);
+            if (results.length === 0) return callback(new Error('[EditorAsset] 未找到指定资源!'), null);
             if (cc.assetManager) {
                 cc.assetManager.loadAny({ uuid: results[0].uuid, type: type }, (_err: Error, _result: T) => {
                     callback(_err, _result);
@@ -30,6 +33,7 @@ export default class EditorAsset {
 
 }
 
+/** 资源信息 */
 interface AssetInfo {
     destPath: string;
     hidden: boolean;

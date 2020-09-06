bc13dd388b7b40ce5a599af784c3dfb619c07ce4
陈皮皮 <952157129@qq.com>
2020-09-06 23:20:56 +0800

    update some effect components.

--- components/effects/GaussianBlur.ts
+++ components/effects/GaussianBlur.ts
@@ -1,39 +1,32 @@
 import EditorAsset from "../../misc/EditorAsset";
 
-const { ccclass, property, executeInEditMode, disallowMultiple, requireComponent, executionOrder } = cc._decorator;
+const { ccclass, property, requireComponent, executeInEditMode, disallowMultiple, executionOrder } = cc._decorator;
 
+/**
+ * 高斯模糊 Shader 组件，该组件需要对应的 Effect 才能正常使用！
+ * @see GaussianBlur.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/components/effects/GaussianBlur.ts
+ * @see eazax-gaussian-blur-adjustable.effect https://gitee.com/ifaswind/eazax-ccc/blob/master/resources/effects/eazax-gaussian-blur-adjustable.effect
+ */
 @ccclass
+@requireComponent(cc.Sprite)
 @executeInEditMode
 @disallowMultiple
-@requireComponent(cc.Sprite)
 @executionOrder(-100)
 export default class GaussianBlur extends cc.Component {
 
-    @property({
-        type: cc.EffectAsset,
-        tooltip: CC_DEV && 'Effect 资源',
-        readonly: true
-    })
-    private effect: cc.EffectAsset = null;
+    @property private _effect: cc.EffectAsset = null;
+    @property({ type: cc.EffectAsset, tooltip: CC_DEV && 'Effect 资源', readonly: true })
+    public get effect() { return this._effect; }
+    public set effect(value: cc.EffectAsset) { this._effect = value; this.init(); }
 
-    @property({ tooltip: CC_DEV && '半径' })
-    private radius: number = 10;
+    @property private _radius: number = 10;
+    @property({ tooltip: CC_DEV && '模糊半径' })
+    public get radius() { return this._radius; }
+    public set radius(value: number) { this._radius = value > 50 ? 50 : value; this.updateProperties(); }
 
-    @property({ tooltip: CC_DEV && '每帧自动更新' })
-    public keepUpdating: boolean = false;
-
-    private material: cc.Material = null; // 材质
-
-    private isIniting: boolean = false; // 是否正在初始化
+    private material: cc.Material = null;
 
     protected onLoad() {
-        // 使用自定义 Effect 需禁用目标贴图的 packable 属性，因为动态合图后无法正确计算纹理 uv
-        // 详情请看：https://docs.cocos.com/creator/manual/zh/asset-workflow/sprite.html#packable
-        let spriteFrame = this.getComponent(cc.Sprite).spriteFrame;
-        if (spriteFrame) spriteFrame.getTexture().packable = false;
-        // 或者全局禁用动态合图功能（不推荐！！！）
-        // cc.dynamicAtlasManager.enabled = false;
-
         this.init();
     }
 
@@ -41,50 +34,43 @@ export default class GaussianBlur extends cc.Component {
         this.init();
     }
 
-    protected update() {
-        if (!this.material || !this.keepUpdating) return;
-        this.render(true);
-    }
-
     /**
-     * 初始化组件
+     * 初始化
      */
-    private async init() {
-        if (this.isIniting) return;
-        this.isIniting = true;
-
-        // 编辑器环境下自动绑定 Effect 资源
-        // 依赖于 EditorAsset 模块，没有模块请将此代码块以及顶部导入语句注释
-        if (CC_EDITOR && !this.effect) {
+    public async init() {
+        /**
+         * 编辑器环境下自动绑定 Effect 资源
+         * 依赖于 EditorAsset 模块，没有模块请将此代码块以及顶部导入语句注释
+         * @see EditorAsset.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/misc/EditorAsset.ts
+         */
+        if (CC_EDITOR && !this._effect) {
             await new Promise(res => {
-                EditorAsset.load('eazax-ccc/resources/effects/eazax-gaussian-blur-adjustable.effect', 'effect', (err: any, result: cc.EffectAsset) => {
-                    if (err) cc.warn('请手动指定组件的 Effect 文件！');
-                    else this.effect = result;
+                EditorAsset.load('eazax-ccc/resources/effects/eazax-gaussian-blur-adjustable.effect', 'effect', (err: Error, result: cc.EffectAsset) => {
+                    if (err) cc.warn('[GaussianBlur]', '请手动指定组件的 Effect 资源！');
+                    else this._effect = result;
                     res();
                 });
             });
         }
-
-        if (this.effect) {
-            this.material = cc.Material.create(this.effect);
-            this.node.getComponent(cc.Sprite).setMaterial(0, this.material);
-            this.render(this.keepUpdating);
-        }
-
-        this.isIniting = false;
+        // 设置
+        if (!this._effect) return;
+        // 使用自定义 Effect 需禁用目标贴图的 packable 属性，因为动态合图后无法正确计算纹理 uv
+        // 详情请看：https://docs.cocos.com/creator/manual/zh/asset-workflow/sprite.html#packable
+        const sprite = this.node.getComponent(cc.Sprite);
+        if (sprite) sprite.spriteFrame.getTexture().packable = false;
+        // 生成并应用材质
+        this.material = cc.Material.create(this._effect);
+        sprite.setMaterial(0, this.material);
+        // 更新 Shader 属性
+        this.updateProperties();
     }
 
     /**
-     * 渲染
-     * @param keepUpdating 是否每帧自动更新
+     * 更新 Shader 属性
      */
-    private render(keepUpdating: boolean) {
-        this.radius = this.radius > 50 ? 50 : this.radius;
-
+    private updateProperties() {
         this.material.setProperty('size', this.getNodeSize());
         this.material.setProperty('radius', this.radius);
-
-        this.keepUpdating = keepUpdating;
     }
 
     /**
@@ -93,4 +79,5 @@ export default class GaussianBlur extends cc.Component {
     private getNodeSize() {
         return cc.v2(this.node.width, this.node.height);
     }
+
 }
--- components/effects/HollowOut.ts
+++ components/effects/HollowOut.ts
@@ -1,81 +1,72 @@
 import EditorAsset from "../../misc/EditorAsset";
 
-const { ccclass, property, executeInEditMode, disallowMultiple, requireComponent, executionOrder } = cc._decorator;
+const { ccclass, property, requireComponent, executeInEditMode, disallowMultiple, executionOrder } = cc._decorator;
 
 /** 镂空形状 */
-export enum Shape {
-
+export enum HollowOutShape {
     /** 矩形 */
     Rect = 1,
-
     /** 圆形 */
     Circle
-
 }
 
+/**
+ * 挖孔 Shader 组件，该组件需要对应的 Effect 才能正常使用！
+ * @see HollowOut.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/components/effects/HollowOut.ts
+ * @see eazax-hollowout.effect https://gitee.com/ifaswind/eazax-ccc/blob/master/resources/effects/eazax-hollowout.effect
+ */
 @ccclass
+@requireComponent(cc.Sprite)
 @executeInEditMode
 @disallowMultiple
-@requireComponent(cc.Sprite)
 @executionOrder(-100)
 export default class HollowOut extends cc.Component {
 
-    @property({
-        type: cc.EffectAsset,
-        tooltip: CC_DEV && 'Effect 资源',
-        readonly: true
-    })
-    private effect: cc.EffectAsset = null;
+    @property private _effect: cc.EffectAsset = null;
+    @property({ type: cc.EffectAsset, tooltip: CC_DEV && 'Effect 资源', readonly: true })
+    public get effect() { return this._effect; }
+    public set effect(value: cc.EffectAsset) { this._effect = value; this.init(); }
 
-    @property private _shape: Shape = Shape.Rect;
-    @property({ type: cc.Enum(Shape), tooltip: CC_DEV && '镂空形状' })
+    @property private _shape: HollowOutShape = HollowOutShape.Rect;
+    @property({ type: cc.Enum(HollowOutShape), tooltip: CC_DEV && '镂空形状' })
     public get shape() { return this._shape; }
-    public set shape(value: Shape) { this._shape = value; this.render(); }
+    public set shape(value: HollowOutShape) { this._shape = value; this.updateProperties(); }
 
     @property private _center: cc.Vec2 = cc.v2();
     @property({ tooltip: CC_DEV && '中心坐标' })
     public get center() { return this._center; }
-    public set center(value: cc.Vec2) { this._center = value; this.render(); }
+    public set center(value: cc.Vec2) { this._center = value; this.updateProperties(); }
 
     @property private _width: number = 300;
-    @property({ tooltip: CC_DEV && '宽', visible() { return this.shape === Shape.Rect; } })
+    @property({ tooltip: CC_DEV && '宽', visible() { return this.shape === HollowOutShape.Rect; } })
     public get width() { return this._width; }
-    public set width(value: number) { this._width = value; this.render(); }
+    public set width(value: number) { this._width = value; this.updateProperties(); }
 
     @property private _height: number = 300;
-    @property({ tooltip: CC_DEV && '高', visible() { return this.shape === Shape.Rect; } })
+    @property({ tooltip: CC_DEV && '高', visible() { return this.shape === HollowOutShape.Rect; } })
     public get height() { return this._height; }
-    public set height(value: number) { this._height = value; this.render(); }
+    public set height(value: number) { this._height = value; this.updateProperties(); }
 
     @property private _round: number = 1;
-    @property({ tooltip: CC_DEV && '圆角半径', visible() { return this.shape === Shape.Rect; } })
+    @property({ tooltip: CC_DEV && '圆角半径', visible() { return this.shape === HollowOutShape.Rect; } })
     public get round() { return this._round; }
-    public set round(value: number) { this._round = value; this.render(); }
+    public set round(value: number) { this._round = value; this.updateProperties(); }
 
     @property private _radius: number = 200;
-    @property({ tooltip: CC_DEV && '半径', visible() { return this.shape === Shape.Circle; } })
+    @property({ tooltip: CC_DEV && '半径', visible() { return this.shape === HollowOutShape.Circle; } })
     public get radius() { return this._radius; }
-    public set radius(value: number) { this._radius = value; this.render(); }
+    public set radius(value: number) { this._radius = value; this.updateProperties(); }
 
     @property private _feather: number = 0.5;
-    @property({ tooltip: CC_DEV && '边缘虚化宽度', visible() { return this.shape === Shape.Circle || this.round > 0; } })
+    @property({ tooltip: CC_DEV && '边缘虚化宽度', visible() { return this.shape === HollowOutShape.Circle || this.round > 0; } })
     public get feather() { return this._feather; }
-    public set feather(value: number) { this._feather = value; this.render(); }
+    public set feather(value: number) { this._feather = value; this.updateProperties(); }
 
     private material: cc.Material = null;
 
     private tweenRes: () => void = null;
 
-    private isIniting: boolean = false;
-
     protected onLoad() {
-        // 使用自定义 Effect 需禁用目标贴图的 packable 属性，因为动态合图后无法正确计算纹理 uv
-        // 详情请看：https://docs.cocos.com/creator/manual/zh/asset-workflow/sprite.html#packable
-        const spriteFrame = this.getComponent(cc.Sprite).spriteFrame;
-        if (spriteFrame) spriteFrame.getTexture().packable = false;
-        // 或者全局禁用动态合图功能（不推荐！！！）
-        // cc.dynamicAtlasManager.enabled = false;
-
         this.init();
     }
 
@@ -87,39 +78,42 @@ export default class HollowOut extends cc.Component {
      * 初始化组件
      */
     private async init() {
-        if (this.isIniting) return;
-        this.isIniting = true;
-
-        // 编辑器环境下自动绑定 Effect 资源
-        // 依赖于 EditorAsset 模块，没有模块请将此代码块以及顶部导入语句注释
-        if (CC_EDITOR && !this.effect) {
+        /**
+         * 编辑器环境下自动绑定 Effect 资源
+         * 依赖于 EditorAsset 模块，没有模块请将此代码块以及顶部导入语句注释
+         * @see EditorAsset.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/misc/EditorAsset.ts
+         */
+        if (CC_EDITOR && !this._effect) {
             await new Promise(res => {
-                EditorAsset.load('eazax-ccc/resources/effects/eazax-hollowout.effect', 'effect', (err: Error, result: cc.EffectAsset) => {
-                    if (err) cc.warn('请手动指定 HollowOut 组件的 Effect 文件！');
-                    else this.effect = result;
+                EditorAsset.load('eazax-ccc/resources/effects/hollowout.effect', 'effect', (err: Error, result: cc.EffectAsset) => {
+                    if (err) cc.warn('[HollowOut]', '请手动指定组件的 Effect 资源！');
+                    else this._effect = result;
                     res();
                 });
             });
         }
-
-        if (this.effect) {
-            this.material = cc.Material.create(this.effect);
-            this.node.getComponent(cc.Sprite).setMaterial(0, this.material);
-            this.render();
-        }
-
-        this.isIniting = false;
+        // 设置
+        if (!this._effect) return;
+        // 使用自定义 Effect 需禁用目标贴图的 packable 属性，因为动态合图后无法正确计算纹理 uv
+        // 详情请看：https://docs.cocos.com/creator/manual/zh/asset-workflow/sprite.html#packable
+        const sprite = this.node.getComponent(cc.Sprite);
+        if (sprite) sprite.spriteFrame.getTexture().packable = false;
+        // 生成并应用材质
+        this.material = cc.Material.create(this._effect);
+        sprite.setMaterial(0, this.material);
+        // 更新 Shader 属性
+        this.updateProperties();
     }
 
     /**
-     * 渲染
+     * 更新 Shader 属性
      */
-    private render() {
+    private updateProperties() {
         switch (this._shape) {
-            case Shape.Rect:
+            case HollowOutShape.Rect:
                 this.rect(this._center, this._width, this._height, this._round, this._feather);
                 break;
-            case Shape.Circle:
+            case HollowOutShape.Circle:
                 this.circle(this._center, this._radius, this._feather);
                 break;
         }
@@ -134,7 +128,7 @@ export default class HollowOut extends cc.Component {
      * @param feather 边缘虚化宽度
      */
     public rect(center?: cc.Vec2, width?: number, height?: number, round?: number, feather?: number) {
-        this._shape = Shape.Rect;
+        this._shape = HollowOutShape.Rect;
 
         if (center !== null) this._center = center;
         if (width !== null) this._width = width;
@@ -164,7 +158,7 @@ export default class HollowOut extends cc.Component {
      * @param feather 边缘虚化宽度
      */
     public circle(center?: cc.Vec2, radius?: number, feather?: number) {
-        this._shape = Shape.Circle;
+        this._shape = HollowOutShape.Circle;
 
         if (center !== null) this._center = center;
         if (radius !== null) this._radius = radius;
@@ -199,7 +193,7 @@ export default class HollowOut extends cc.Component {
             if (round > height / 2) round = height / 2;
             if (feather > round) feather = round;
 
-            this._shape = Shape.Rect;
+            this._shape = HollowOutShape.Rect;
 
             cc.tween<HollowOut>(this)
                 .to(time, {
@@ -236,7 +230,7 @@ export default class HollowOut extends cc.Component {
             this.tweenRes && this.tweenRes();
             this.tweenRes = res;
 
-            this._shape = Shape.Circle;
+            this._shape = HollowOutShape.Circle;
 
             cc.tween<HollowOut>(this)
                 .to(time, {

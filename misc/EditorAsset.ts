ae68061c37ce19df84d3f6c5766681b83866f0a9
陈皮皮 <952157129@qq.com>
2020-09-22 19:58:02 +0800

    update misc/EditorAsset.ts.

--- misc/EditorAsset.ts
+++ misc/EditorAsset.ts
@@ -6,25 +6,26 @@ export default class EditorAsset {
 
     /**
      * 在编辑器环境中加载 assets 目录下的资源
-     * @param url 相对路径，根目录为 assets/，注意带上文件后缀，如：eazax-sine-wave.effect
-     * @param type 资源类型，如：effect
+     * @param path 相对路径，根目录为 assets/，注意带上文件后缀，如：eazax-sine-wave.effect
+     * @param assetType 资源类型，如：effect
      * @param callback 加载完成回调
      * @example 
-     *  EditorAsset.load('eazax-ccc/resources/effects/eazax-sine-wave.effect', 'effect', (err: Error, result: cc.EffectAsset) => {
+     *  const path = 'eazax-ccc/resources/effects/eazax-sine-wave.effect';
+     *  EditorAsset.load(path, 'effect', (err: Error, result: cc.EffectAsset) => {
      *      this.effect = result;
      *  });
      */
-    public static load<T>(url: string, type: string, callback: (err: Error, result: T) => void): void {
+    public static load<T>(path: string, assetType: string, callback: (err: Error, result: T) => void): void {
         if (!CC_EDITOR) return cc.warn('[EditorAsset]', '该函数只在编辑器环境内有效！');
-        Editor.assetdb.queryAssets(`db://assets/${url}`, type, (err: Error, results: AssetInfo[]) => {
+        Editor.assetdb.queryAssets(`db://assets/${path}`, assetType, (err: Error, results: AssetInfo[]) => {
             if (err) return callback(new Error('[EditorAsset] 未知错误!'), null);
             if (results.length === 0) return callback(new Error('[EditorAsset] 未找到指定资源!'), null);
-            if (cc.assetManager) {
-                cc.assetManager.loadAny({ uuid: results[0].uuid, type: type }, (_err: Error, _result: T) => {
+            if (cc.assetManager && cc.assetManager.loadAny) {
+                cc.assetManager.loadAny({ uuid: results[0].uuid, type: assetType }, (_err: Error, _result: T) => {
                     callback(_err, _result);
                 });
             } else {
-                cc.loader.load({ uuid: results[0].uuid, type: type }, (_err: Error, _result: T) => {
+                cc.loader.load({ uuid: results[0].uuid, type: assetType }, (_err: Error, _result: T) => {
                     callback(_err, _result);
                 });
             }

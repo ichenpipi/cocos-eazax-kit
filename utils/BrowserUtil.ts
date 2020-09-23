569b116d8318a253eda6806397a835dc149ecc5c
陈皮皮 <952157129@qq.com>
2020-09-23 19:36:50 +0800

    update utils/BrowserUtil.ts.

--- utils/BrowserUtil.ts
+++ utils/BrowserUtil.ts
@@ -1,13 +1,32 @@
 /**
  * 浏览器工具
+ * @see BrowserUtil.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/utils/BrowserUtil.ts
  */
 export default class BrowserUtil {
 
     /**
-     * 获取当前 URL 参数
+     * 清除当前 URL 的参数（修改历史记录，不会刷新当前网页）
+     */
+    public static clearUrlParam(): void {
+        if (!window || !window.history) return;
+        window.history.replaceState({}, null, '.');
+    }
+
+    /**
+     * 设置当前 URL 的参数（修改历史记录，不会刷新当前网页）
+     * @param param 参数
+     */
+    public static setUrlParam(param: string): void {
+        if (!window || !window.history) return;
+        window.history.replaceState({}, null, `?${param}`);
+    }
+
+    /**
+     * 获取当前 URL 的参数
      * @param key 键
      */
-    public static getUrlParam(key: string) {
+    public static getUrlParam(key: string): string {
+        if (!window || !window.location) return;
         const query = window.location.search.replace('?', '');
         if (query === '') return null;
         const keyValues = query.split('&');
@@ -22,7 +41,8 @@ export default class BrowserUtil {
      * 复制文本至设备剪贴板
      * @param value 文本内容
      */
-    public static copy(value: string) {
+    public static copy(value: string): boolean {
+        if (!document) return false;
         // 创建输入元素
         let element = document.createElement('textarea');
         element.readOnly = true;

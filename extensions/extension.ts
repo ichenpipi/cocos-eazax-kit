5c95acbcc14a28733ff64478df1e2fba9106192b
陈皮皮 <952157129@qq.com>
2020-09-25 19:08:37 +0800

    update extensions/extension.ts.

--- extensions/extension.ts
+++ extensions/extension.ts
@@ -1,7 +1,5 @@
-// String.prototype.translate = function () {
-//     // TODO
-//     return this;
-// };
+// 类型扩展
+// @see extension.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/extensions/extension.ts
 
 String.prototype.clamp = function (start, threshold, suffix = '...') {
     if (this.replace(/[^\x00-\xff]/g, 'xx').length <= threshold) return this;
@@ -14,3 +12,8 @@ String.prototype.clamp = function (start, threshold, suffix = '...') {
     }
     return result;
 };
+
+// String.prototype.translate = function () {
+//     // TODO
+//     return this;
+// };

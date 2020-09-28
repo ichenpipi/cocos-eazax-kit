e0a4b3127b027106fd0ee4576d94bbd588144faf
陈皮皮 <952157129@qq.com>
2020-09-28 12:45:00 +0800

    update utils/PromiseUtil.ts.

--- utils/PromiseUtil.ts
+++ utils/PromiseUtil.ts
@@ -1,11 +1,12 @@
 /**
  * Promise 工具
+ * @see PromiseUtil.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/utils/PromiseUtil.ts
  */
 export default class PromiseUtil {
 
     /**
      * 等待
-     * @param time 时间（秒）
+     * @param time 时间（单位：秒）
      */
     public static delay(time: number): Promise<void> {
         return new Promise(res => setTimeout(res, time * 1000));

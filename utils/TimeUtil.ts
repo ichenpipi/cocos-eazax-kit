3be3c0189eacc65a2e96f8dd2c568b396bf828fe
陈皮皮 <952157129@qq.com>
2020-09-28 11:43:30 +0800

    update utils/TimeUtil.ts.

--- utils/TimeUtil.ts
+++ utils/TimeUtil.ts
@@ -1,5 +1,6 @@
 /**
  * 时间工具
+ * @see TimeUtil.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/utils/TimeUtil.ts
  */
 export default class TimeUtil {
 
@@ -8,11 +9,28 @@ export default class TimeUtil {
      * @param hour 时
      * @param minute 分
      * @param second 秒
+     * @example
+     * const time = TimeUtil.getTargetTimestamp(10, 20, 30); // 1601259630000
+     * const timeString = new Date(time).toLocaleString(); // "上午10:20:30"
      */
     public static getTargetTimestamp(hour: number = 0, minute: number = 0, second: number = 0): number {
         const start = new Date(new Date().toLocaleDateString()).getTime();
         const target = ((hour * 3600) + (minute * 60) + second) * 1000;
         return new Date(start + target).getTime();
+    }
+
+    /**
+     * 将毫秒转为时分秒的格式（最小单位为秒，如：”00:01:59“）
+     * @param time 毫秒数
+     * @param separator 分隔符
+     * @example 
+     * const HMS = TimeUtil.msToHMS(119000); // "00:01:59"
+     */
+    public static msToHMS(time: number, separator: string = ':'): string {
+        const hours = Math.floor(time / 3600000);
+        const minutes = Math.floor((time - (hours * 3600000)) / 60000);
+        const seconds = Math.floor((time - (hours * 3600000) - (minutes * 60000)) / 1000);
+        return `${hours.toString().padStart(2, '0')}${separator}${minutes.toString().padStart(2, '0')}${separator}${seconds.toString().padStart(2, '0')}`;
     }
 
     /**
@@ -22,16 +40,6 @@ export default class TimeUtil {
         return new Date().getTime();
     }
 
-    /**
-     * 将毫秒转为时分秒的格式（最小单位为秒，如：”00:01:59“）
-     * @param time 毫秒数
-     * @param separator 分隔符
-     */
-    public static msToHMS(time: number, separator: string = ':'): string {
-        const hours = Math.floor(time / 3600000);
-        const minutes = Math.floor((time - (hours * 3600000)) / 60000);
-        const seconds = Math.floor((time - (hours * 3600000) - (minutes * 60000)) / 1000);
-        return `${hours.toString().padStart(2, '0')}${separator}${minutes.toString().padStart(2, '0')}${separator}${seconds.toString().padStart(2, '0')}`;
-    }
-
 }
+
+window.eazax['TimeUtil'] = TimeUtil;

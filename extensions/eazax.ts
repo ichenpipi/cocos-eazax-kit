91e2c08177277e2ba7b968a558356f264b6f2d10
陈皮皮 <952157129@qq.com>
2020-09-28 12:39:07 +0800

    update extensions/eazax.ts.

--- extensions/eazax.ts
+++ extensions/eazax.ts
@@ -24,55 +24,7 @@ namespace eazax {
         }
     }
 
-    /**
-     * 展示动态图集
-     * @param status 状态
-     */
-    export function showDynamicAtlas(status: boolean = true): cc.Node {
-        return cc.dynamicAtlasManager.showDebug(status);
-    }
-
-    /**
-     * 展示左下角的统计面板
-     * @param status 状态
-     */
-    export function showStats(status: boolean = true): void {
-        cc.debug.setDisplayStats(status);
-    }
-
-    /**
-     * 更改统计面板的文本颜色
-     * @param font 文本颜色
-     */
-    export function setStatsColor(font: cc.Color = cc.Color.WHITE, background: cc.Color = cc.color(0, 0, 0, 150)) {
-        const profiler = cc.find('PROFILER-NODE');
-        if (!profiler) return cc.warn('未找到统计面板节点！');
-
-        // 文字
-        profiler.children.forEach(node => node.color = font);
-
-        // 背景
-        let node = profiler.getChildByName('BACKGROUND');
-        if (!node) {
-            node = new cc.Node('BACKGROUND');
-            profiler.addChild(node, cc.macro.MIN_ZINDEX);
-            node.setContentSize(profiler.getBoundingBoxToWorld());
-            node.setPosition(0, 0);
-        }
-        const graphics = node.getComponent(cc.Graphics) || node.addComponent(cc.Graphics);
-        graphics.clear();
-        graphics.rect(-5, 12.5, node.width + 10, node.height - 10);
-        graphics.fillColor = background;
-        graphics.fill();
-    }
-
-    /**
-     * 上一次渲染帧所提交的渲染批次总数
-     */
-    export function getDrawCalls(): number {
-        return cc.renderer.drawCalls;
-    }
-
 }
 
-window.eazax = eazax;
+window['eazax'] = eazax;
+window['ez'] = window['eazax'];

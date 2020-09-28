6a099e552de371e1986a96185682d04f6c3b1d36
陈皮皮 <952157129@qq.com>
2020-09-28 10:43:09 +0800

    update utils/TimeUtil.ts.

--- utils/TimeUtil.ts
+++ utils/TimeUtil.ts
@@ -10,10 +10,9 @@ export default class TimeUtil {
      * @param second 秒
      */
     public static getTargetTimestamp(hour: number = 0, minute: number = 0, second: number = 0): number {
-        let start = new Date(new Date().toLocaleDateString()).getTime();
-        let target = ((hour * 3600) + (minute * 60) + second) * 1000;
-        let result = new Date(start + target).getTime();
-        return result;
+        const start = new Date(new Date().toLocaleDateString()).getTime();
+        const target = ((hour * 3600) + (minute * 60) + second) * 1000;
+        return new Date(start + target).getTime();
     }
 
     /**
@@ -29,9 +28,9 @@ export default class TimeUtil {
      * @param separator 分隔符
      */
     public static msToHMS(time: number, separator: string = ':'): string {
-        let hours = Math.floor(time / 3600000);
-        let minutes = Math.floor((time - (hours * 3600000)) / 60000);
-        let seconds = Math.floor((time - (hours * 3600000) - (minutes * 60000)) / 1000);
+        const hours = Math.floor(time / 3600000);
+        const minutes = Math.floor((time - (hours * 3600000)) / 60000);
+        const seconds = Math.floor((time - (hours * 3600000) - (minutes * 60000)) / 1000);
         return `${hours.toString().padStart(2, '0')}${separator}${minutes.toString().padStart(2, '0')}${separator}${seconds.toString().padStart(2, '0')}`;
     }
 

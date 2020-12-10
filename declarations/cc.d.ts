700894fe8aea44b3a619f74b2a07da784993f04f
陈皮皮 <952157129@qq.com>
2020-12-10 18:13:55 +0800

    update declarations/cc.d.ts.

--- declarations/cc.d.ts
+++ declarations/cc.d.ts
@@ -1,15 +1,46 @@
 /**
- * cc 命名空间扩展
+ * 扩展 cc 模块，声明一些 creator.d.ts 中没有声明（但实际上有）的东西~
  * @author 陈皮皮（ifaswind）
- * @version 20200509
+ * @version 20201210
  * @see https://gitee.com/ifaswind/eazax-ccc/blob/master/declarations/cc.d.ts
  */
 declare namespace cc {
 
+    interface TouchOneByOne {
+
+        _claimedTouches: Array;
+
+        swallowTouches: boolean;
+
+        onTouchBegan: Function;
+
+        onTouchMoved: Function;
+
+        onTouchEnded: Function;
+
+        onTouchCancelled: Function;
+
+        setSwallowTouches(needSwallow: boolean): void;
+
+        isSwallowTouches(): boolean;
+
+        clone(): TouchOneByOne;
+
+        checkAvailable(): boolean;
+
+    }
+
+    interface Node {
+
+        _touchListener?: TouchOneByOne;
+
+    }
+
     /**
-     * 资源库类
+     * 资源库类（cc.AssetLibrary 在 v2.4 中被移除）
+     * @deprecated
      */
-    export class AssetLibrary {
+    class AssetLibrary {
 
         static loadAsset(uuid: string, callback: (err, result) => void, options?: { existingAsset?: any });
 

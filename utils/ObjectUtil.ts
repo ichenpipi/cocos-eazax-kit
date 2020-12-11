b5103c79e8bbeb4b82c1128cd1681d4b0c409f8c
陈皮皮 <952157129@qq.com>
2020-12-11 12:48:24 +0800

    update utils/ObjectUtil.ts.

--- utils/ObjectUtil.ts
+++ utils/ObjectUtil.ts
@@ -13,47 +13,45 @@ export default class ObjectUtil {
     }
 
     /**
-    * 深拷贝
-    * @param target 目标对象
-    */
+     * 深拷贝
+     * @param target 目标对象
+     */
     public static deepCopy(target: object): any {
-        let result: any;
+        if (target == null || typeof target !== 'object') {
+            return target;
+        }
 
-        // Handle the 3 simple types, and null or undefined
-        if (target == null || typeof target != "object") return target;
+        let result = null;
 
-        // Handle Date
         if (target instanceof Date) {
             result = new Date();
             result.setTime(target.getTime());
             return result;
         }
 
-        // Handle Array
         if (target instanceof Array) {
             result = [];
-            for (let i = 0, len = target.length; i < len; i++) {
+            for (let i = 0, length = target.length; i < length; i++) {
                 result[i] = this.deepCopy(target[i]);
             }
             return result;
         }
 
-        // Handle Object
         if (target instanceof Object) {
             result = {};
-            for (let attribute in target) {
-                if (target.hasOwnProperty(attribute)) result[attribute] = this.deepCopy(target[attribute]);
+            for (const key in target) {
+                if (target.hasOwnProperty(key)) {
+                    result[key] = this.deepCopy(target[key]);
+                }
             }
             return result;
         }
-
-        throw new Error("Unable to copy target! Its type isn't supported.");
     }
 
     /**
-    * 返回目标对象的拷贝（仅基本类型）
-    * @param target 目标对象
-    */
+     * 返回目标对象的拷贝（仅基本类型）
+     * @param target 目标对象
+     */
     public static copy(target: object): object {
         // 1
         // let result = {...target};
@@ -62,10 +60,11 @@ export default class ObjectUtil {
         // let result = JSON.parse(JSON.stringify(target));
 
         // 3
-        let result = {};
+        const result = {};
         for (let name in target) {
             result[name] = target[name];
         }
         return result;
     }
+
 }

6068ace9fa43adc7422d44547867491a975e7c4f
陈皮皮 <952157129@qq.com>
2021-01-17 19:22:14 +0800

    update declarations/cc.d.ts.

--- declarations/cc.d.ts
+++ declarations/cc.d.ts
@@ -1,10 +1,16 @@
 /**
  * 扩展 cc 模块，声明一些 creator.d.ts 中没有声明（但实际上有）的东西~
  * @author 陈皮皮（ifaswind）
- * @version 20201210
+ * @version 20210117
  * @see https://gitee.com/ifaswind/eazax-ccc/blob/master/declarations/cc.d.ts
  */
-declare namespace cc {
+declare module cc {
+
+    interface Node {
+
+        _touchListener?: TouchOneByOne;
+
+    }
 
     interface TouchOneByOne {
 
@@ -30,23 +36,53 @@ declare namespace cc {
 
     }
 
-    interface Node {
+    interface RenderComponent {
 
-        _touchListener?: TouchOneByOne;
+        _materials: cc.Material,
+
+        _assembler: any;
+
+        _vertsDirty: any;
+
+        _resetAssembler();
+
+        __preload();
+
+        setVertsDirty();
+
+        _on3DNodeChanged();
+
+        _validateRender();
+
+        markForValidate();
+
+        markForRender();
+
+        disableRender();
+
+        _getDefaultMaterial();
+
+        _activateMaterial();
+
+        _updateMaterial();
+
+        _updateColor();
+
+        _checkBacth();
 
     }
 
     /**
-     * 资源库类（cc.AssetLibrary 在 v2.4 中被移除）
-     * @deprecated
+     * 资源库模块
+     * @deprecated cc.AssetLibrary 已在 v2.4 中被移除
      */
-    class AssetLibrary {
+    module AssetLibrary {
 
-        static loadAsset(uuid: string, callback: (err, result) => void, options?: { existingAsset?: any });
+        function loadAsset(uuid: string, callback: (err, result) => void, options?: { existingAsset?: any });
 
-        static queryAssetInfo(uuid: string, callback: (err, result) => void);
+        function queryAssetInfo(uuid: string, callback: (err, result) => void);
 
-        static getAssetByUuid(uuid: string);
+        function getAssetByUuid(uuid: string);
 
     }
 

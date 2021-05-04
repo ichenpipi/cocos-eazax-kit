08cc0f32fb1abc80af11e8573ca666fcef4bfe45
陈皮皮 <952157129@qq.com>
2021-05-04 17:30:02 +0800

    update components/BackgroundFitter.ts.

--- components/BackgroundFitter.ts
+++ components/BackgroundFitter.ts
@@ -1,47 +1,60 @@
 import EventManager from "../core/EventManager";
-import { VIEW_RESIZE } from "../constants/Events";
 
-const { ccclass } = cc._decorator;
+const { ccclass, executionOrder } = cc._decorator;
 
 /**
  * 背景适配组件
  * @see BackgroundFitter.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/components/BackgroundFitter.ts
+ * @version 20210504
  */
 @ccclass
+@executionOrder(-99)
 export default class BackgroundFitter extends cc.Component {
 
     protected onLoad() {
-        EventManager.on(VIEW_RESIZE, this.adapt, this);
+        this.registerEvent();
     }
 
-    protected start() {
+    protected onEnable() {
         this.adapt();
     }
 
     protected onDestroy() {
-        EventManager.off(VIEW_RESIZE, this.adapt, this);
+        this.unregisterEvent();
+    }
+
+    /**
+     * 订阅事件
+     */
+    protected registerEvent() {
+        EventManager.on('view-resize', this.adapt, this);
+    }
+
+    /**
+     * 取消事件订阅
+     */
+    protected unregisterEvent() {
+        EventManager.off('view-resize', this.adapt, this);
     }
 
     /**
      * 适配
      */
-    private adapt() {
-        let screenRatio = cc.winSize.height / cc.winSize.width;
-        let designRatio = cc.Canvas.instance.designResolution.height / cc.Canvas.instance.designResolution.width;
+    protected adapt() {
+        // 实际屏幕比例
+        const winSize = cc.winSize,
+            screenRatio = winSize.height / winSize.width;
+        // 设计比例
+        const designResolution = cc.Canvas.instance.designResolution,
+            designRatio = designResolution.height / designResolution.width;
 
+        let scale = 1;
         if (screenRatio >= designRatio) {
-            let scale = cc.winSize.height / cc.Canvas.instance.designResolution.height;
-            this.node.scale = scale;
+            scale = winSize.height / designResolution.height;
         } else {
-            let scale = cc.winSize.width / cc.Canvas.instance.designResolution.width;
-            this.node.scale = scale;
+            scale = winSize.width / designResolution.width;
         }
-
-        // cc.log('winSize', cc.winSize)
-        // cc.log('visibleSize ', cc.view.getVisibleSize())
-        // cc.log('getVisibleSizeInPixel ', cc.view.getVisibleSizeInPixel())
-        // cc.log('getFrameSize', cc.view.getFrameSize())
-        // cc.log('designResolution', cc.Canvas.instance.designResolution)
+        this.node.scale = scale;
     }
 
 }

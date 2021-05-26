8cdded77b68779269afed0321aabe2a3ca6e3ae5
陈皮皮 <952157129@qq.com>
2021-05-26 11:29:24 +0800

    update core/EventManager.ts.

--- core/EventManager.ts
+++ core/EventManager.ts
@@ -1,66 +1,84 @@
 /**
  * 事件管理器
  * @see EventManager.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/core/EventManager.ts
- * @example 
- * EventManager.on('start', this.onStart, this);
- * EventManager.emit('start');
+ * @version 20210421
+ * @example
+ * // 注册事件
+ * EventManager.on('game-start', this.onGameStart, this);
+ * // 发射事件
+ * EventManager.emit('game-start', 666);
  */
 export default class EventManager {
 
+    /**
+     * 普通事件容器
+     */
     private static events: Map<string, Subscription[]> = new Map<string, Subscription[]>();
 
+    /**
+     * 一次性事件容器
+     */
     private static onceEvents: Map<string, Subscription[]> = new Map<string, Subscription[]>();
 
     /**
-     * 监听事件
+     * 注册事件
      * @param name 事件名
      * @param callback 回调
-     * @param target 订阅对象
+     * @param target 目标
      */
     public static on(name: string, callback: Function, target?: any) {
-        if (!this.events.has(name)) {
-            this.events.set(name, []);
+        const events = this.events;
+        if (!events.has(name)) {
+            events.set(name, [{ callback, target }]);
+            return;
         }
-        this.events.get(name).push({ callback, target });
+        events.get(name).push({ callback, target });
     }
 
     /**
-     * 监听事件（一次性）
+     * 注册事件（一次性）
      * @param name 事件名
      * @param callback 回调
-     * @param target 订阅对象
+     * @param target 目标
      */
     public static once(name: string, callback: Function, target?: any) {
-        if (!this.onceEvents.has(name)) {
-            this.onceEvents.set(name, []);
+        const events = this.onceEvents;
+        if (!events.has(name)) {
+            events.set(name, [{ callback, target }]);
+            return;
         }
-        this.onceEvents.get(name).push({ callback, target });
+        events.get(name).push({ callback, target });
     }
 
     /**
-     * 取消监听事件
+     * 取消注册事件
      * @param name 事件名
      * @param callback 回调
-     * @param target 订阅对象
+     * @param target 目标
      */
     public static off(name: string, callback: Function, target?: any) {
-        if (this.events.has(name)) {
-            const subscriptions = this.events.get(name);
-            for (let i = 0; i < subscriptions.length; i++) {
-                if (subscriptions[i].target === target &&
-                    (subscriptions[i].callback === callback || subscriptions[i].callback.toString() === callback.toString())) {
-                    subscriptions.splice(i, 1);
+        // 普通事件
+        const event = this.events.get(name);
+        if (event) {
+            for (let i = 0, l = event.length; i < l; i++) {
+                if (this.compare(event[i], callback, target)) {
+                    event.splice(i, 1);
+                    if (event.length === 0) {
+                        this.events.delete(name);
+                    }
                     break;
                 }
             }
         }
         // 一次性事件
-        if (this.onceEvents.has(name)) {
-            const subscriptions = this.onceEvents.get(name);
-            for (let i = 0; i < subscriptions.length; i++) {
-                if (subscriptions[i].target === target &&
-                    (subscriptions[i].callback === callback || subscriptions[i].callback.toString() === callback.toString())) {
-                    subscriptions.splice(i, 1);
+        const onceEvent = this.onceEvents.get(name);
+        if (onceEvent) {
+            for (let i = 0, l = onceEvent.length; i < l; i++) {
+                if (this.compare(onceEvent[i], callback, target)) {
+                    onceEvent.splice(i, 1);
+                    if (onceEvent.length === 0) {
+                        this.onceEvents.delete(name);
+                    }
                     break;
                 }
             }
@@ -68,35 +86,40 @@ export default class EventManager {
     }
 
     /**
-     * 发射事件
+     * 通过事件名发送事件
      * @param name 事件名
      * @param args 参数
      */
     public static emit(name: string, ...args: any[]) {
-        if (this.events.has(name)) {
-            const subscriptions = this.events.get(name);
-            for (let i = 0; i < subscriptions.length; i++) {
-                subscriptions[i].callback.apply(subscriptions[i].target, args);
+        // 普通事件
+        const event = this.events.get(name);
+        if (event) {
+            for (let i = 0; i < event.length; i++) {
+                const { callback, target } = event[i];
+                callback.apply(target, args);
             }
         }
         // 一次性事件
-        if (this.onceEvents.has(name)) {
-            let subscriptions = this.onceEvents.get(name);
-            for (let i = 0; i < subscriptions.length; i++) {
-                subscriptions[i].callback.apply(subscriptions[i].target, args);
+        const onceEvent = this.onceEvents.get(name);
+        if (onceEvent) {
+            for (let i = 0; i < onceEvent.length; i++) {
+                const { callback, target } = onceEvent[i];
+                callback.apply(target, args);
             }
-            subscriptions.length = 0;
+            this.onceEvents.delete(name);
         }
     }
 
     /**
-     * 移除事件
+     * 移除指定事件
      * @param name 事件名
      */
     public static remove(name: string) {
+        // 普通事件
         if (this.events.has(name)) {
             this.events.delete(name);
         }
+        // 一次性事件
         if (this.onceEvents.has(name)) {
             this.onceEvents.delete(name);
         }
@@ -106,14 +129,29 @@ export default class EventManager {
      * 移除所有事件
      */
     public static removeAll() {
+        // 普通事件
         this.events.clear();
+        // 一次性事件
         this.onceEvents.clear();
+    }
+
+    /**
+     * 对比
+     * @param subscription 订阅
+     * @param inCallback 回调
+     * @param inTarget 目标
+     */
+    private static compare(subscription: Subscription, inCallback: Function, inTarget: any) {
+        const { callback, target } = subscription;
+        return target === inTarget && (callback === inCallback || callback.toString() === inCallback.toString());
     }
 
 }
 
 /** 订阅 */
 interface Subscription {
+    /** 回调 */
     callback: Function;
+    /** 目标 */
     target: any;
 }

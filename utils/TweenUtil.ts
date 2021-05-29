1467d247c986eca75ca97b6df74976cebcd2cdc1
陈皮皮 <952157129@qq.com>
2021-05-30 00:13:30 +0800

    update utils/TweenUtil.ts.

--- utils/TweenUtil.ts
+++ utils/TweenUtil.ts
@@ -1,11 +1,12 @@
 /**
  * Tween 工具
  * @see TweenUtil.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/utils/TweenUtil.ts
+ * @version 20210320
  */
 export default class TweenUtil {
 
     /**
-     * 水平翻转（翻牌）
+     * 水平翻转（卡片翻转）
      * @param node 节点
      * @param duration 总时长
      * @param onMiddle 中间状态回调
@@ -13,20 +14,21 @@ export default class TweenUtil {
      */
     public static flip(node: cc.Node, duration: number, onMiddle?: Function, onComplete?: Function): Promise<void> {
         return new Promise<void>(res => {
-            const time = duration / 2;
-            const skew = 10;
-            cc.tween(node)
+            const tween = cc.tween,
+                time = duration / 2,
+                scaleX = node.scale,
+                skewY = scaleX > 0 ? 20 : -20;
+            tween(node)
                 .parallel(
-                    cc.tween().to(time, { scaleX: 0 }, { easing: 'sineIn' }),
-                    cc.tween().to(time, { skewY: -skew }),
+                    tween().to(time, { scaleX: 0 }, { easing: 'quadIn' }),
+                    tween().to(time, { skewY: -skewY }, { easing: 'quadOut' }),
                 )
-                .set({ skewY: skew })
                 .call(() => {
                     onMiddle && onMiddle();
                 })
                 .parallel(
-                    cc.tween().to(time, { scaleX: 1 }, { easing: 'sineOut' }),
-                    cc.tween().to(time, { skewY: 0 }),
+                    tween().to(time, { scaleX: -scaleX }, { easing: 'quadOut' }),
+                    tween().to(time, { skewY: 0 }, { easing: 'quadIn' }),
                 )
                 .call(() => {
                     onComplete && onComplete();

c3c0cddd3de6bb0052dbcb8613acab61058de84d
陈皮皮 <952157129@qq.com>
2021-12-05 06:38:18 +0000

    update misc/EditorAsset.ts.

--- misc/EditorAsset.ts
+++ misc/EditorAsset.ts
@@ -1,5 +1,7 @@
 /**
  * 编辑器内资源加载类
+ * @author 陈皮皮 (ifaswind)
+ * @version 20211205
  * @see EditorAsset.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/misc/EditorAsset.ts
  */
 export default class EditorAsset {
@@ -15,20 +17,36 @@ export default class EditorAsset {
      *      this.effect = result;
      *  });
      */
-    public static load<T>(path: string, assetType: string, callback: (err: Error, result: T) => void): void {
-        if (!CC_EDITOR) return cc.warn('[EditorAsset]', '该函数只在编辑器环境内有效！');
-        Editor.assetdb.queryAssets(`db://assets/${path}`, assetType, (err: Error, results: AssetInfo[]) => {
-            if (err) return callback(new Error('[EditorAsset] 未知错误!'), null);
-            if (results.length === 0) return callback(new Error('[EditorAsset] 未找到指定资源!'), null);
-            if (cc.assetManager && cc.assetManager.loadAny) {
-                cc.assetManager.loadAny({ uuid: results[0].uuid, type: assetType }, (_err: Error, _result: T) => {
-                    callback(_err, _result);
-                });
-            } else {
-                cc.loader.load({ uuid: results[0].uuid, type: assetType }, (_err: Error, _result: T) => {
-                    callback(_err, _result);
-                });
+    public static load<T>(path: string, assetType: string, callback?: (err: Error, result: T) => void): Promise<T> {
+        return new Promise(res => {
+            if (!CC_EDITOR) {
+                res(null);
+                cc.warn('[EditorAsset]', '该函数只在编辑器环境内有效！');
+                return;
             }
+            Editor.assetdb.queryAssets(`db://assets/${path}`, assetType, (err: Error, results: AssetInfo[]) => {
+                if (err) {
+                    res(null);
+                    callback && callback(new Error('[EditorAsset] 未知错误!'), null);
+                    return;
+                }
+                if (results.length === 0) {
+                    res(null);
+                    callback && callback(new Error('[EditorAsset] 未找到指定资源!'), null);
+                    return;
+                }
+                if (cc.assetManager && cc.assetManager.loadAny) {
+                    cc.assetManager.loadAny({ uuid: results[0].uuid, type: assetType }, (_err: Error, _result: T) => {
+                        res(_result);
+                        callback && callback(_err, _result);
+                    });
+                } else {
+                    cc.loader.load({ uuid: results[0].uuid, type: assetType }, (_err: Error, _result: T) => {
+                        res(_result);
+                        callback && callback(_err, _result);
+                    });
+                }
+            });
         });
     }
 

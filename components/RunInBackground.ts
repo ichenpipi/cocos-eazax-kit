8b0c49f5d9da5457a12c969d4fe5e0a42acc1d93
陈皮皮 <952157129@qq.com>
2022-02-06 17:51:26 +0000

    update components/RunInBackground.ts.

--- components/RunInBackground.ts
+++ components/RunInBackground.ts
@@ -1,35 +1,101 @@
-const { ccclass } = cc._decorator;
+const { ccclass, property, executionOrder, help } = cc._decorator;
 
+/**
+ * 用于在浏览器后台保持运行
+ * @author 陈皮皮 (ifaswind)
+ * @version 20220207
+ * @see RunInBackground.ts https://gitee.com/ifaswind/eazax-ccc/blob/master/components/RunInBackground.ts
+ */
 @ccclass
+@executionOrder(-1)
+@help('https://gitee.com/ifaswind/eazax-ccc/blob/master/components/RunInBackground.ts')
 export default class RunInBackground extends cc.Component {
 
+    @property({ displayName: CC_DEV && '脚本地址', tooltip: CC_DEV && 'Worker 脚本地址' })
+    private url: string = '/worker.js';
+
+    /** Worker 实例 */
     private worker: Worker = null;
 
-    private url: string = "Worker.js";
-
+    /**
+     * 生命周期：加载
+     */
     protected onLoad() {
-        if (CC_DEBUG) this.url = "app/editor/static/preview-templates/Worker.js";
+        this.init();
+        this.registerEvent();
+    }
 
-        document.addEventListener("visibilitychange", () => {
-            if (document.visibilityState === "hidden") {
-                if (cc.game["_paused"]) cc.game["resume"]();
-                this.worker = new Worker(this.url);
-                this.worker.onmessage = () => { cc.director["mainLoop"](); }
-            } else if (document.visibilityState === "visible") {
-                if (this.worker) this.worker.terminate();
+    /**
+     * 生命周期：销毁
+     */
+    protected onDestroy() {
+        this.unregisterEvent();
+    }
+
+    /**
+     * 注册事件
+     */
+    protected registerEvent() {
+        this.onVisibilityChange = this.onVisibilityChange.bind(this);
+        document.addEventListener('visibilitychange', this.onVisibilityChange);
+    }
+
+    /**
+     * 反注册事件
+     */
+    protected unregisterEvent() {
+        document.removeEventListener('visibilitychange', this.onVisibilityChange);
+    }
+
+    /**
+     * 初始化
+     */
+    private init() {
+        // 设为常驻节点
+        this.node.setParent(cc.director.getScene());
+        cc.game.addPersistRootNode(this.node);
+        // 网页调试需要在预览模板目录下放一份 worker.js
+        // 如果使用编辑器自带的预览模板，还需要修改脚本地址
+        // if (CC_DEBUG) {
+        //     this.url = '/app/editor/static/preview-templates/worker.js';
+        // }
+    }
+
+    /**
+     * 页面切换回调
+     */
+    private onVisibilityChange() {
+        // 切换到后台
+        if (document.visibilityState === 'hidden') {
+            // 确保引擎处于运行状态
+            if (cc.game.isPaused()) {
+                cc.game.resume();
             }
-        });
+            // 创建工作线程
+            this.worker = new Worker(this.url);
+            this.worker.onmessage = () => {
+                // 调用 Cocos 引擎主循环
+                cc.director['mainLoop']();
+            }
+        }
+        // 切换到前台
+        else if (document.visibilityState === 'visible') {
+            if (this.worker) {
+                this.worker.terminate();
+                this.worker = null;
+            }
+        }
     }
 
 }
 
 /*
 
-以下为 Worker.js 脚本内容：
+以下为 worker.js 脚本内容：
 
 function call(){
-	postMessage(1);
-	setTimeout("call()", 1000 / 60);
+    postMessage(1);
+    setTimeout('call()', 1000 / 60);
 }
 call();
 
